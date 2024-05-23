a82bd23d19450ea03d2ad8806c2b4e0495ba2ee4
CalimanNPI <danteshield10@gmail.com>
2024-05-23 09:24:53 -0600

    Envio emails

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -549,11 +549,11 @@
                       </label>
 
                       <textarea
-                        id="message"
-                        class="mt-2 w-full rounded-lg border-gray-200 align-top shadow-sm sm:text-sm"
-                        rows="4"
-                        placeholder="Mensaje ..."
-                        v-model="message"
+                      class="mt-2 w-full rounded-lg border-gray-200 align-top shadow-sm sm:text-sm"
+                      rows="4"
+                      placeholder="Mensaje ..."
+                      v-model="message"
+                      id="message"
                         name="message"
                       ></textarea>
                     </div>
